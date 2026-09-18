2ae647c25048c8eba7bef16985057f0293602583
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-18 12:09:15 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.DIARIO_CONFIG = {
+  // Endereço público que devolverá as listas actualizadas em JSON.
+  masterDataUrl: "",
+
+  // Endereço que receberá cada submissão do formulário.
+  submissionUrl: "",
+
+  // Enquanto os endereços estiverem vazios, o site funciona em modo de demonstração.
+  demoMode: true
+};

37ae10bd28ef9ff9c6ca65eeab9bb6f1b6d33384
sirmotors4-questionarios <sirmotors4@gmail.com>
2026-09-20 14:25:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.DIARIO_CONFIG = {
   masterDataUrl: "",
 
   // Endereço HTTPS do Power Automate que receberá cada submissão.
-  submissionUrl: "COLE_AQUI_O_HTTP_URL_DO_POWER_AUTOMATE",
+  submissionUrl: "https://default5fc7bb0c4c3f4008ae039efd62fc96.c9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/15/workflows/80ced2a22712418f903383d3a86c4994/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=zbbjBDHybVXMwhMniw1qw3RDRreUJkVkX42diD2R9II",
 
   // Coloque false para usar o envio real.
   demoMode: false
